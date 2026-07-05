f7c4c0005b0b7d25b936c7527a31f14216f22a6d
RupenderSinghRathore <kamisamakirishima@gmail.com>
2026-07-05 11:12:48 +0530

    refactor: log to appropriate file

--- src/logger.rs
+++ src/logger.rs
@@ -1,23 +1,34 @@
 use std::{
-    fs::{File, OpenOptions},
+    env,
+    fmt::Display,
+    fs::{File, OpenOptions, create_dir_all},
     io::Write,
+    path::PathBuf,
     sync::{LazyLock, Mutex},
 };
 
-const LOG_FILE_PATH: &str = "diagnostic.log";
+use crate::APP_NAME;
+
+const LOG_FILE_PATH: &str = "app.log";
 
 static LOG: LazyLock<Mutex<File>> = LazyLock::new(|| {
+    let mut path = PathBuf::from(env::var("HOME").unwrap());
+    path.push(".local/state");
+    path.push(APP_NAME);
+    create_dir_all(&path).unwrap();
+
+    path.push(LOG_FILE_PATH);
     Mutex::new(
         OpenOptions::new()
             .create(true)
             .write(true)
             .truncate(true)
-            .open(LOG_FILE_PATH)
+            .open(path)
             .unwrap(),
     )
 });
 
-pub fn log(msg: impl AsRef<str>) {
+pub fn log(msg: impl Display) {
     let mut file = LOG.lock().unwrap();
-    writeln!(file, "{}", msg.as_ref()).ok();
+    writeln!(file, "{}", msg.to_string()).ok();
 }
